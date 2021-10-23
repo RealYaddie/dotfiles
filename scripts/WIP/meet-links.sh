eaec93752fd24072e550ad2613f87c890d7da74a
RealYaddie <alphaleo777@gmail.com>
2021-10-22 19:53:05 -0500

    Cleaned up meet-links script

--- scripts/WIP/meet-links.sh
+++ scripts/WIP/meet-links.sh
@@ -2,35 +2,16 @@
 
 # DESCRIPTION: Simple dmenu prompt that will display all my class and club google meet links and on selecting one it'll launch it in default browser.
 
-#TODO: Figure out how to "pretty" up the output that is passed to dmenu so that the entire path isn't showing.
 #TODO: Figure out how to source dmenurc and use the settings from there in this script, so dmenu looks the same everywhere.
 
 # The Directory with all the Quick launch links for year 3
 directory="$HOME/Desktop/CMU Docs/Year 3 Semester 1(21-22)/Quick Launch"
 
-# choice="$(ls "$directory/"*.desktop* | sed 's/[.]desktop//' | dmenu -l 20)"
-
-# Finds all desktop links in the directory and lists them out in a dmenu prompt
-# choice="$(find "$directory" -maxdepth 1 -name '*.desktop'| sed 's#^/h.*Launch$##' | dmenu -i -l 20)"
-# choice="$(find "$directory" -maxdepth 1 -name '*.desktop' | sed -e 's#^/h.*Launch/##g' -e 's#[.]desktop$##g' | dmenu -i -l 20)"
+# Finds all desktop links in the directory and lists them out in a dmenu prompt with the full path and .desktop extension removed in the dmenu prompt
 prompt="$(find "$directory" -maxdepth 1 -name '*.desktop' | sed -e 's#^/h.*Launch/##g' -e 's#[.]desktop$##g' | dmenu -i -l 20)"
+
+# Adds the full path and the .desktop extension to whatever option was chosen from the dmenu prompt.
 choice="$(echo $directory/$prompt.desktop)"
 
-# prompt="$(echo "$choice" | sed 's#^/h.*Launch/##g' | dmenu -i -l 20)"
-
 # Grab only the link from the option that was chosen.
-# link=$( awk '/.*https.*/ {print $0}' "$choice" | sed 's/^URL=//' | xargs -r -I{} brave "{}")
-link=$( awk '/.*https.*/ {print $0}' "$choice" | sed 's/^URL=//')
-
-# xargs -I{} brave "{}"
-# brave "$link"
-echo "The link is:"
-echo $link
-# echo "The directory is "$directory
-# echo "The choice is "$choice
-echo ""
-echo "The choice is:"
-echo $choice
-echo ""
-echo "Output from prompt is:"
-echo $prompt
+link=$( awk '/.*https.*/ {print $0}' "$choice" | sed 's/^URL=//' | xargs -r -I{} brave "{}")
