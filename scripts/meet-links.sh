d023f014a3715e282768f6f8e5a2012530fb643a
RealYaddie <alphaleo777@gmail.com>
2021-11-19 02:48:27 -0500

    Added some font rendering stuff to Xresources
    Fixed up dmenurc and properly sourced it in 2 scripts

--- scripts/meet-links.sh
+++ scripts/meet-links.sh
@@ -2,14 +2,20 @@
 
 # DESCRIPTION: Simple dmenu prompt that will display all my class and club google meet links and on selecting one it'll launch it in default browser.
 
-# TODO: Figure out how to source dmenurc and use the settings from there in this script, so dmenu looks the same everywhere.
 # TODO: Put the meeting code after the name of the meet just so I can quickly check the links.
+
+# Source dmenurc if it exists and sets a default if it isn't present
+if [ -f $HOME/.dmenurc ]; then
+    . $HOME/.dmenurc
+else
+    DMENU='dmenu -i'
+fi
 
 # The Directory with all the Quick launch links for year 3
 directory="$HOME/Desktop/CMU Docs/Year 3 Semester 1(21-22)/Quick Launch"
 
 # Finds all desktop links in the directory and lists them out in a dmenu prompt with the full path and .desktop extension removed in the dmenu prompt
-prompt="$(find "$directory" -maxdepth 1 -name '*.desktop' | sed -e 's#^/h.*Launch/##g' -e 's#[.]desktop$##g' | dmenu -i -l 20)"
+prompt="$(find "$directory" -maxdepth 1 -name '*.desktop' | sed -e 's#^/h.*Launch/##g' -e 's#[.]desktop$##g' | $DMENU -l 20)"
 
 # Adds the full path and the .desktop extension to whatever option was chosen from the dmenu prompt.
 choice="$(echo $directory/$prompt.desktop)"
