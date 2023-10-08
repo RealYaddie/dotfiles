3307940a3e4ba4e5900da29f9667c6194a237cab
RealYaddie <alphaleo777@gmail.com>
2023-10-08 11:46:37 -0500

    Made new alias for cover letter and resume folders. Added new TODO to mdmenu script.

--- .scripts/mdmenu.sh
+++ .scripts/mdmenu.sh
@@ -3,8 +3,7 @@
 # Simple dmenu script that takes a youtube video link and opens it up in mpv, just for the times where you don't want to
 # have to open up a terminal.
 
-# TODO: Add the different resolution options and options for playlists as well.
-#       Choose what resolution to play the video in
+# TODO: Play YT vids at 1080p otherwise just play the video normally using mpv. Because if its not a YT link and YT options are sent to mpv then the video won't play
 
 # Grab the YT video URL from the clipboard with xsel
 URL=$(xsel -b -o)
